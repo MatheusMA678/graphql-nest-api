12e80b7cda93867a054cb45784e6501b34f7ccd8
MatheusMA678 <matheusma2510@hotmail.com>
2023-09-09 12:18:19 -0400

    fix: back cors to true

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    methods: ['POST', 'OPTIONS', 'GET'],
-    origin: ['*'],
-    credentials: true
-  })
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT);
 }
 bootstrap();
